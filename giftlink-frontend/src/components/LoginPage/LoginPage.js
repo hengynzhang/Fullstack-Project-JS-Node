5bb1296e5e9e9076d65ed7366d0b2962163d6f36
hengynzhang <hengyuanzhang9612@gmail.com>
2024-10-05 04:26:58 -0400

    login and register page

---
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -0,0 +1,53 @@
+import React, {useState} from 'react';
+import './LoginPage.css';
+import {Link} from 'react-router-dom';
+
+function LoginPage () {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        console.log('Login successfully!')
+    }
+
+    return (
+        <div className='container mt-5'>
+            <div className='row justify-content-center'>
+                <div className='col-md-6 col-lg-4'>
+                    <form className='login-card p-4 border rounder' onSubmit={handleLogin}>
+                        <h2 className='text-cneter mb-4 font-weight-bold'>Login</h2>
+                        <div className='mb-3'>
+                            <label className='form-label'>Email</label>
+                            <input 
+                                type='email'
+                                namae='email'
+                                value={email}
+                                placeholder='Enter your email'
+                                onChange={(e) => setEmail(e.target.value)}
+                                className='form-control input'/>
+                        </div>
+                        <div>
+                            <label className='form-label'>Password</label>
+                            <input 
+                                type='password'
+                                name='password'
+                                value={password}
+                                placeholder='Enter your password'
+                                onChange={(e) => setPassword(e.target.value)}
+                                className='form-control input'/>
+                        </div>
+                        <button type='submit' className='btn btn-primary'>Login</button>
+                        <p className='mt-4 text-center'>New here? 
+                            <Link href='/app/register' className='text-primary'>Register Here</Link>
+                        </p>
+                    </form>
+                </div>
+                
+            </div>
+            
+        </div>
+    )
+}
+
+export default LoginPage;
--- giftlink-frontend/src/components/RegisterPage/RegisterPage.js
+++ giftlink-frontend/src/components/RegisterPage/RegisterPage.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import './RegisterPage.css';
+import {Link} from 'react-router-dom';
 
 function RegisterPage() {
     const [firstName, setFirstName] = useState('');
@@ -7,67 +8,68 @@ function RegisterPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleRegister = async () => {
+    const handleRegister = async (e) => {
+        e.preventDefault();
         console.log('Registered successfully!')
+    }
 
     return (
         <div className='container mt-5'>
             <div className='row justify-content-center'>
                 <div className='col-md-6 col-lg-4'>
-                    <div className='register-card p-4 border rounded'>
+                    <form className='register-card p-4 border rounded' onSubmit={handleRegister}>
                         <h2 className='text-center mb-4 font-weight-bold'>Register</h2>
                         <div className='mb-4'>
-                            <label className='form-label'>firstName</label>
+                            <label className='form-label'>First Name</label>
                             <input 
                                 type='text'
-                                className='input' 
-                                placeholder='Enter your firstName'
+                                className='form-control input' 
+                                placeholder='Enter your First Name'
                                 name='firstName' 
                                 value={firstName}
                                 onChange= {(e)=> {setFirstName(e.target.value)}}/>   
                         </div>
                         <div className='mb-4'>
-                            <label className='form-label'>lastName</label>
+                            <label className='form-label'>Last Name</label>
                             <input 
                                 type='text'
-                                className='input' 
-                                placeholder='Enter your lastName'
+                                className='form-control input' 
+                                placeholder='Enter your Last Name'
                                 name='lastName' 
                                 value={lastName}
                                 onChange= {(e)=> {setLastName(e.target.value)}}/>  
                         </div>
                         <div className='mb-4'>
-                            <label className='form-label'>email</label>
+                            <label className='form-label'>Email</label>
                             <input 
                                 type='email'
-                                className='input' 
+                                className='form-control input' 
                                 placeholder='Enter your email'
                                 name='email' 
                                 value={email}
                                 onChange= {(e)=> {setEmail(e.target.value)}}/>
                         </div>
                         <div className='mb-4'>
-                            <label className='form-label'>password</label>
+                            <label className='form-label'>Password</label>
                             <input 
                                 type='password'
-                                className='input' 
+                                className='form-control input' 
                                 placeholder='Enter your password'
                                 name='password' 
                                 value={password}
                                 onChange= {(e)=> {setPassword(e.target.value)}}/>
                         </div>
                         
-                        <button className='btn btn-primary' onClick={handleRegister}>Register</button>
+                        <button type='submit' className='btn btn-primary'>Register</button>
                         <p className='mt-4 text-center'>
                             Already a member?
-                            <a href='/login' className='text-primary'>Login</a>
+                            <Link to='/app/login' className='text-primary'>Login</Link>
                         </p>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
     )
-}
 }
 
 export default RegisterPage;
